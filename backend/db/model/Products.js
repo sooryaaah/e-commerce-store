53af17bbfed619f13ad4c9ac4dcd4c472aa8a751
sooryams <sooryamsns@gmail.com>
2025-11-22 00:53:39 +0530

    products

---
+++ backend/db/model/Products.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    image: {
+      secure_url: {
+        type: String,
+      },
+      publicId: {
+        type: String,
+      },
+      required: [true, "image is required"],
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/authController.js";
+import { login, logout, signup, refreshToken } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup)
-router.get("/login", login)
-router.get("/logout", logout)
+router.post("/login", login)
+router.post("/logout", logout)
+router.post("/refresh-token", refreshToken)
 
 export default router
 
