c5e9b01fc751e268bf845ecb64d1bf78d2914b36
sooryams <sooryamsns@gmail.com>
2025-12-12 19:54:41 +0530

    ready for deployemnt

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,15 @@ import paymentRoutes from "./routes/paymentRoutes.js"
 import analyticsRoutes from "./routes/analyticsRoutes.js"
 import { mongoconnect } from "./db/connect.js";
 import cors from "cors"
+import path from "path"
 
 
 dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -30,6 +33,14 @@ app.use("/api/cart", cartRoutes)
 app.use("/api/coupons", couponRoutes)
 app.use("/api/payments", paymentRoutes  )
 app.use("/api/analytics", analyticsRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 
 app.listen(PORT, ()=>{
