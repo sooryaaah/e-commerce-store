2426d5d1311e8b3b80c282541b3dbe335fb4e35f
sooryams <sooryamsns@gmail.com>
2025-12-13 23:29:06 +0530

    use

--- backend/app.js
+++ backend/app.js
@@ -34,12 +34,14 @@ app.use("/api/coupons", couponRoutes)
 app.use("/api/payments", paymentRoutes  )
 app.use("/api/analytics", analyticsRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-    app.get("/*", (req, res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
+    app.use((req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "frontend", "dist", "index.html")
+        );
+    });
 }
 
 
