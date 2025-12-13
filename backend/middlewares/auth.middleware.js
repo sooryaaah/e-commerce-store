9e38b48d972d3b3f87ead041e33ce2ff65da8c41
sooryams <sooryamsns@gmail.com>
2025-12-13 22:44:35 +0530

    change user import

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../db/model/User.js";
+import User from "../db/model/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
